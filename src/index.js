83c07853ef97ae47e25e7d3ec4b00b3fabed73f2
unknown <578331299@qq.com>
2019-05-15 12:05:29 +0800

    test app code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import ReactDOM from 'react-dom'; // 挂载在节点上 root
+// 大写开头，都是组件 App组件
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
